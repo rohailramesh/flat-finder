5c22a23b13cdcbd12732359fddf893f194151264
Rohail Ramesh <r.ramesh@se21.qmul.ac.uk>
2023-03-15 16:20:22 +0000

    folder reorganisation

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,22 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Collapse from "react-bootstrap/Collapse";
+import LoginReg from "./loginregpage";
 
 export default function Home() {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
   return (
     <>
-      <Button
-        onClick={() => setOpen(!open)}
-        login-controls="collapse-login"
-        login-expanded={open}
-      >
-        Login
-      </Button>
-      <Collapse in={open}>
-        <div id="collapse-login">Please login to Flatify!</div>
-      </Collapse>
+      <LoginReg />
     </>
   );
 }
