a8e4ade62d33f00ac11164edffc627d0133896c8
giabertu <giabertu99@gmail.com>
2023-03-16 17:42:44 +0000

    feat: added login flow

--- src/services/user.js
+++ src/services/user.js
@@ -14,10 +14,18 @@ export default class User {
     }
     })
 
-    console.log({data, error})
-    return data
+    console.log("Registration result: ", {data, error})
+    return data.user
   }
 
+  async login(supabase, email, password){
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email,
+      password
+    })
+    console.log("Login result: ", {data, error})
+    return data.user
+  }
 
   async getAuthUser(supabase){
     const { data } = await supabase.auth.getUser()
