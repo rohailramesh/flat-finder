31e6ae6ce035803a83864fe363ec2999f22d4f04
haquangminhan <haquangminhan@gmail.com>
2023-03-20 18:18:16 +0000

    add addTicket function in TicketService

---
+++ src/services/TicketService.js
@@ -0,0 +1,24 @@
+import { Ticket } from "@/models/Ticket";
+
+export default class TicketService{
+
+    url = 'http://127.0.0.1:3001'
+
+    constructor() {}
+
+    async addTicket(title, content){
+        const response = await fetch(`${url}/ticket`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                title, 
+                content
+            })
+        })
+        
+        return response
+    }
+
+}
